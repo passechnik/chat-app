faf09b7e9833f00152c87e16ea9f6fc726bbf378
Valerie Kucher <passechnik214@gmail.com>
2023-04-05 16:05:13 +0300

    add chat section with messages

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 const Message = () => {
     return (
-        <div>Message</div>
+        <div className="message">
+            <div className="message-info">
+                <img src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" />
+                <span>just now</span>
+            </div>
+            <div className="message-content">
+                <p>hello</p>
+                {/* <img src="https://images.unsplash.com/photo-1552374196-c4e7ffc6e126?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8cG9ydHJhaXR8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60" alt="" /> */}
+            </div>
+        </div>
     )
 }
 
